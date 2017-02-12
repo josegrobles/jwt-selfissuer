9ea25c8cd479d1e0bbdeb97d6d6165f84a26f386
Jose Antonio González Robles <josegrobles1997@gmail.com>
2017-02-12 12:41:24 +0100

    Improved quality of tests and response info

--- test/test.js
+++ test/test.js
@@ -3,50 +3,74 @@ var httpMocks = require('node-mocks-http'),
 
 var should = require('chai').should()
 const key = process.env.JWT_KEY
+var token
 
-describe('Token handling',function(){
+describe('Token handling - With Expired Token', function() {
 
-  const authorization = 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFzZGFzYWRzYSIsInBlcm1pc3Npb25zIjoidXNlciIsImV4cCI6MTQ4NjgxOTMyNiwiaWF0IjoxNDg2ODE4NDI2fQ.8EYW8b1vxcEGeZJaPGkhSSpuYSUSrfW4bD3p1kbo5oM'
+    const authorization = 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFzZGFzYWRzYSIsInBlcm1pc3Npb25zIjoidXNlciIsImV4cCI6MTQ4NjgxOTMyNiwiaWF0IjoxNDg2ODE4NDI2fQ.8EYW8b1vxcEGeZJaPGkhSSpuYSUSrfW4bD3p1kbo5oM'
 
-  it('Check token generated',function(done){
-    var req  = httpMocks.createRequest({
+    var req = httpMocks.createRequest({
         method: 'POST',
         headers: {
-          authorization
+            authorization
         }
     });
 
     var res = httpMocks.createResponse({
-      eventEmitter: require('events').EventEmitter
+        eventEmitter: require('events').EventEmitter
     });
-    var f1 = handler(key)
-    f1(req,res)
-    res.on('end', function() {
-       const data = JSON.parse(res._getData())
-        data.should.have.property('token')
+
+    var data
+
+    it('Check token generated', function(done) {
+        var f1 = handler(key)
+        f1(req, res)
+        res.on('end', function() {
+            data = JSON.parse(res._getData())
+            data.should.have.property('token')
+            done()
+        })
+    })
+
+    it('Check expire date', function(done) {
+        token = data.token
+        data.should.have.property('expire')
         done()
-     })
+    })
 })
 
-it('Check expire date',function(done){
-  var req  = httpMocks.createRequest({
-      method: 'POST',
-      headers: {
-        authorization
-        }
+
+describe('Token handling - With Working Token', function() {
+    var req,res,data,authorization
+
+    it('Check if error info is provided', function(done) {
+
+      authorization = 'JWT ' + token
+
+      req = httpMocks.createRequest({
+          method: 'POST',
+          headers: {
+              authorization
+          }
       });
 
-  var res = httpMocks.createResponse({
-    eventEmitter: require('events').EventEmitter
-  });
-  var f1 = handler(key)
+      res = httpMocks.createResponse({
+          eventEmitter: require('events').EventEmitter
+      });
 
-  f1(req,res)
-  res.on('end', function() {
-     const data = JSON.parse(res._getData())
-      data.should.have.property('token')
-      done()
-   })
-})
+        let f1 = handler(key)
+        f1(req, res)
+
+        res.on('end', function() {
+            data = JSON.parse(res._getData())
+            data.should.have.property('status')
+            done()
+        })
+    })
+
+    it('Check expire date', function(done) {
+        data.should.have.property('message')
+        done()
+    })
 
 })

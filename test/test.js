3bf685be18cd4cbaac252fdb20ebd11c13b5d087
Jose Antonio González Robles <josegrobles1997@gmail.com>
2017-02-13 23:15:02 +0100

    Promisified for async token issuing

--- test/test.js
+++ test/test.js
@@ -71,7 +71,7 @@ describe('Token handling - With Working Token', function() {
         var f1 = handler(key)
         f1(req, res, ok)
     })
-    
+
     it('Check info is correctly stored - username', function(done) {
       const info = req.info
       info.should.have.property('username')

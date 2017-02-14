7f83b5a1a337932dfc65f31b9bcd3b018326bb4d
Jose Antonio González Robles <josegrobles1997@gmail.com>
2017-02-14 20:47:48 +0100

    Improved tests

--- test/test.js
+++ test/test.js
@@ -17,73 +17,71 @@ client.on('connect', () => {
 });
 
 describe('Token handling - With Expired Token', function() {
-    var req,res,token,hash
+    var req, res, token, hash
 
+    before(function(done) {
+        hash = crypto.randomBytes(8)
+        hash = hash.toString('hex')
+        info = {
+            username: "test",
+            permissions: "user",
+            hash
+        }
+        info.exp = Math.floor(Date.now() / 1000)
+        jwt.sign(info, key, {}, function(err,token) {
+            let uuid = crypto.randomBytes(32)
+            uuid = uuid.toString('hex')
+            client.hset("test_hashes", uuid, hash, function(err, response) {
+                const authorization = "JWT " + token
+                req = httpMocks.createRequest({
+                    method: 'POST',
+                    headers: {
+                        authorization
+                    }
+                });
+                res = httpMocks.createResponse();
+                done()
+            })
+        })
+    });
 
-    it('Create token', function(done) {
-      hash = crypto.randomBytes(8)
-      hash = hash.toString('hex')
-      info = {username:"test",permissions:"user",hash}
-      info.exp = Math.floor(Date.now()/1000)
-      token = jwt.sign(info, key)
-      done()
-    })
-
-
-    it('Store token along hash', function(done) {
-      let uuid = crypto.randomBytes(32)
-      uuid = uuid.toString('hex')
-      client.hset("test_hashes",uuid,hash, (err,response) =>{
+    after(function(done){
+      client.del("test_hashes",function(err,response){
         done()
       })
     })
 
-    it('Create Request and response mock', function(done) {
-      const authorization = "JWT "+ token
-
-      req = httpMocks.createRequest({
-          method: 'POST',
-          headers: {
-              authorization
-          }
-      });
-
-      res = httpMocks.createResponse();
-
-      done()
-    })
 
     it('Check token generated', function(done) {
-      function ok(){
-        var data = res._headers
-        tokenG = data['X-Token']
-        assert.property(data,'X-Token')
-        done()
-      }
-        var f1 = handler(key,client)
+        function ok() {
+            var data = res._headers
+            tokenG = data['X-Token']
+            assert.property(data, 'X-Token')
+            done()
+        }
+        var f1 = handler(key, client)
         f1(req, res, ok)
     })
 
     it('Check info is correctly stored - username', function(done) {
-      const info = req.info
-      info.should.have.property('username').and.equal('test')
-      done()
+        const info = req.info
+        info.should.have.property('username').and.equal('test')
+        done()
     })
 
     it('Check info is correctly stored - permissions', function(done) {
-      const info = req.info
-      info.should.have.property('permissions').and.equal('user')
-      done()
+        const info = req.info
+        info.should.have.property('permissions').and.equal('user')
+        done()
     })
 
 })
 
 
 describe('Token handling - With Working Token', function() {
-  var authorization,req,res
+    var authorization, req, res
 
-
-    it('Check that no token is generated', function(done) {
+    before(function(){
       authorization = 'JWT ' + tokenG
 
       req = httpMocks.createRequest({
@@ -94,42 +92,46 @@ describe('Token handling - With Working Token', function() {
       });
 
       res = httpMocks.createResponse();
+    })
 
-      function ok(){
-        var data = res._headers
-        assert.equal(data['X-Token'],tokenG)
-        done()
-      }
-        var f1 = handler(key,client)
+    it('Check that no token is generated', function(done) {
+        function ok() {
+            var data = res._headers
+            assert.equal(data['X-Token'], tokenG)
+            done()
+        }
+        var f1 = handler(key, client)
         f1(req, res, ok)
     })
 
     it('Check info is correctly stored - username', function(done) {
-      const info = req.info
-      info.should.have.property('username').and.equal('test')
-      done()
+        const info = req.info
+        info.should.have.property('username').and.equal('test')
+        done()
     })
 
     it('Check info is correctly stored - permissions', function(done) {
-      const info = req.info
-      info.should.have.property('permissions').and.equal('user')
-      done()
+        const info = req.info
+        info.should.have.property('permissions').and.equal('user')
+        done()
     })
 
 })
 
 describe('Token handling - Without Token being sent', function() {
-    var req,res,data
+    var req, res, data
 
-    it('Check if error info is provided', function(done) {
-
+    before(function(){
       req = httpMocks.createRequest({
           method: 'POST'
       });
 
       res = httpMocks.createResponse();
+    })
 
-        let f1 = handler(key,client)
+    it('Check if error info is provided', function(done) {
+
+        let f1 = handler(key, client)
         f1(req, res)
 
         data = JSON.parse(res._getData())
